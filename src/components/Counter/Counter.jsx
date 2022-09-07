819327097672fab8890947396cff8e21e9ddbe4c
BrookVerse <brookewaldorf90@gmail.com>
2022-09-07 14:02:40 +0100

    Added Buttons, with some logic.

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Counter.scss";
+const count = document.querySelector(".count");
+let number = 0;
 
 const Counter = () => {
+  const handleplusOne = () => {
+    number++;
+    return count.innerHTML = number.toString();
+  };
   return (
     <div className="counter">
-      <p>0</p>
+      <p className="count">0</p>
       <div className="counter__text">
-        <p>-</p>
-        <p>+</p>
+        <button className="counter__sub">-</button>
+        <button onClick={handleplusOne} className="counter__add">
+          +
+        </button>
       </div>
     </div>
   );
--- src/components/TrackerContainer/TrackerContainer.jsx
+++ src/components/TrackerContainer/TrackerContainer.jsx
@@ -1,19 +1,13 @@
-import React from 'react'
-import "./TrackerContainer.scss"
-import TrackerCard from '../TrackerCard/TrackerCard'
-import employees from "../../assets/data/employee"
+import React from "react";
+import "./TrackerContainer.scss";
+import TrackerCard from "../TrackerCard/TrackerCard";
+import employees from "../../assets/data/employee";
 
 const TrackerContainer = () => {
-  const employeeCards = employees.map((employee)=> {
-    return(
-      <TrackerCard name={employee.name} role={employee.role} />
-    )
-  })
-  return (
-    <div className='tracker'>
-        {employeeCards}
-    </div>
-  )
-}
+  const employeeCards = employees.map((employee) => {
+    return <TrackerCard key={employee.id} name={employee.name} role={employee.role} />;
+  });
+  return <div className="tracker">{employeeCards}</div>;
+};
 
-export default TrackerContainer
+export default TrackerContainer;
